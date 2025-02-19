e82fb40f5094022d58f0f78acae8b21217e7479d
João Pedro O. Braga <ojpbraga@gmail.com>
2025-02-19 19:44:20 -0300

    fix: calculator overflow-hidden

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header';
+import Keyboard from './Keyboard';
 
 function Calculator() {
   return (
-    <div className='bg-black/65 h-92 w-60 rounded-xl drop-shadow-2xl'>
+    <div className='bg-black/65 h-92 w-60 rounded-xl drop-shadow-2xl flex flex-col justify-between overflow-hidden'>
         <Header/>
+
+        <Keyboard/>
     </div>
   )
 };
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Keyboard() {
+  return (
+    <div className='bg-amber-700 w-full h-[60%]'>
+
+    </div>
+  )
+}
+
+export default Keyboard
