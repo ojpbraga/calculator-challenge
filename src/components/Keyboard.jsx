d4b861a2edd54180255219335432f6d13aaa9b53
João Pedro O. Braga <ojpbraga@gmail.com>
2025-02-19 20:59:10 -0300

    feat: Keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 function Keyboard() {
-  return (
-    <div className='bg-amber-700 w-full h-[60%]'>
+    const buttons_left = ['AC','AC','AC','1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    const button_right = ['/','X', '-', '+', '=']
 
+
+  return (
+    <div className='w-full h-[65%] flex gap-1 p-2'>
+
+        <div className='grid grid-cols-3 gap-1 flex-1 items-center'>
+            {
+                buttons_left.map(button => (
+                    <div className='grid place-items-center w-full h-full bg-white/65 rounded-full font-semibold cursor-pointer'>{button}</div>
+                ))
+            }
+        </div>
+
+        <div className='h-full w-[23%] grid items-center gap-1'>
+            {
+                button_right.map(button => (
+                    <div className='grid place-items-center w-full h-full  rounded-full font-bold bg-amber-300 cursor-pointer'>{button}</div>
+                ))
+            }
+            
+        </div>
+       
     </div>
   )
 }
