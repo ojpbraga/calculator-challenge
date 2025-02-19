afdc0aca3e7172fe9c10a86ddde51bcba916d019
João Pedro O. Braga <ojpbraga@gmail.com>
2025-02-19 19:17:41 -0300

    style: background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
+import background from '/public/images/wallpaper.jpg'
+
 function App() {
 
   return (
-    <>
+    <main style={{
+      backgroundImage: `url(${background})`}} className='h-[100dvh] w-[100dvw] bg-no-repeat bg-cover'>
       
-    </>
+    </main>
   )
 }
 
