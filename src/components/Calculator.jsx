e7347b33bae77caa07bf4e83f3645a42bdebb8bb
João Pedro O. Braga <ojpbraga@gmail.com>
2025-02-20 10:34:45 -0300

    feat: validations I

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,17 +4,32 @@ import Keyboard from './Keyboard';
 import Display from './Display';
 
 function Calculator() {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState([]);
+    const [isCalculate, setIsCalculate] = useState(false);
+
+    function handleClear() {
+        setValue([]);
+    }
     
     useEffect(() => {
+        if(isCalculate){
+            let result = eval(value.join(''));
+            // setValue([]);
+            // setValue([...value, result]);
+            setValue(result);
+            setIsCalculate(false);
+        };
+    }, [isCalculate]);
+
+    useEffect(() => {
         console.log(value)
-    }, [value]);
+    }, [value])
 
   return (
     <div className='bg-black/65 h-92 w-60 rounded-xl drop-shadow-2xl flex flex-col justify-between overflow-hidden p-2'>
         <Header/>
         <Display value={value}/>
-        <Keyboard value={value} setValue={setValue}/>
+        <Keyboard value={value} setValue={setValue} setIsCalculate={setIsCalculate} isCalculate={isCalculate} handleClear={handleClear}/>
     </div>
   )
 };
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,30 +1,78 @@
 import React from 'react'
 
-function Keyboard({value, setValue}) {
-    const buttons_left = ['AC','AC','AC','1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    const button_right = ['/','X', '-', '+', '=']
+function Keyboard({value, setValue, setIsCalculate, isCalculate, handleClear}) {
+    const buttonsNumbers = [
+        { value: 'AC', element: 'AC' },
+        { value: 'AC', element: 'AC' },
+        { value: 'AC', element: 'AC' },
+        { value: '1', element: '1' },
+        { value: '2', element: '2' },
+        { value: '3', element: '3' },
+        { value: '4', element: '4' },
+        { value: '5', element: '5' },
+        { value: '6', element: '6' },
+        { value: '7', element: '7' },
+        { value: '8', element: '8' },
+        { value: '9', element: '9' },
+        { value: '0', element: '0' }
+    ];
+    
+    const buttonsMath = [
+        {value:'/', element: '/'},
+        {value:'*', element: 'X'},
+        {value:'-', element: '-'},
+        {value:'+', element: '+'},
+        {value:'=', element: '='},
+    ];
 
+    const methodsMath = ['/', '*', '-', '+'];
+
+    // Ao dar um click, verifica as condições
     function handleClick({target}){
-        setValue(target.innerText);
+        console.log(target.value, target.innerText)
+        if(verifyValue(target.value, target.innerText)) {
+            console.log("ok")
+            setValue([...value, target.value]);
+        }
+    };
+
+    // Verificação dos valores
+    function verifyValue(valueButton, elementButton) {
+        
+        if(methodsMath.includes(valueButton) && value[0] === undefined) {
+            return false;
+
+        } else if(methodsMath.includes(value[value.length - 1]) && methodsMath.includes(valueButton))  {
+            return false;
+
+        } else if (valueButton === '=' && !methodsMath.includes(value[value.length - 1])) {
+            setIsCalculate(true);
+            return false;
+        } else {
+            return true;
+        }
     }
+
+
 
   return (
     <div className='w-full h-[65%] flex gap-1'>
 
         <div className='grid grid-cols-3 gap-1 flex-1 items-center'>
             {
-                buttons_left.map(button => (
-                    <div onClick={handleClick} className='grid place-items-center w-full h-full bg-white/65 rounded-full font-semibold cursor-pointer'>{button}</div>
+                buttonsNumbers.map(({value, element}) => (
+                    <button onClick={handleClick} className='grid place-items-center w-full h-full bg-white/65 rounded-full font-semibold cursor-pointer' value={value} key={value}>{element}</button>
                 ))
             }
         </div>
 
         <div className='h-full w-[23%] grid items-center gap-1'>
             {
-                button_right.map(button => (
-                    <div onClick={handleClick} className='grid place-items-center w-full h-full  rounded-full font-bold bg-amber-300 cursor-pointer'>{button}</div>
+                buttonsMath.map(({value, element}) => (
+                    <button onClick={handleClick} className='grid place-items-center w-full h-full  rounded-full font-bold bg-amber-300 cursor-pointer' key={value}value={value}>{element}</button>
                 ))
             }
+            {/* <div onClick={handleClick} className='grid place-items-center w-full h-full  rounded-full font-bold bg-amber-300 cursor-pointer'>=</div> */}
             
         </div>
        
